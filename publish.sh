e21ac6333c4b5f2e1a881382091827d0833d61bd
Francesc Vilardell <vilardell@ieec.cat>
2021-12-23 21:55:02 +0100

    Correct 404 page

--- publish.sh
+++ publish.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
 rm -r docs/*
+sed -r 's/(href|src)="/\1="\//' _build/html/404.html | sed -r 's/(href|src)="\/http/\1="http/' > _build/html/404.tmp
+mv _build/html/404.tmp _build/html/404.html
 cp -pr _build/html/* docs/
+echo "supportastronomer.net\c" > docs/CNAME
