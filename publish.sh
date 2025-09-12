b6052b9959ac3a8012a4a3e7487b6bed9151b82e
Francesc Vilardell Sallés <vilardellsalles@gmail.com>
2025-09-12 17:18:14 +0200

    Repository needs to be public to have a usable site

--- publish.sh
+++ publish.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-
+ 
 rm -r docs/*
 sed -r 's/(href|src)="/\1="\//' _build/html/404.html | sed -r 's/(href|src)="\/http/\1="http/' > _build/html/404.tmp
 mv _build/html/404.tmp _build/html/404.html
